729e2c35ccc0b7a447079e86fae01395b1aad9d0
Konrad <abramowski.konrad@gmail.com>
2020-04-15 20:05:08 +0200

    Deviding message into packets implemented

---
+++ ARQ.py
@@ -0,0 +1,14 @@
+import random
+list = []
+msgsize = int(input("Podaj ilosc bitow do wyslania: "))
+packetsize = int(input("Podaj wielkosc pojedynczego pakietu: "))
+packetnumer = int(0)
+for i in range(msgsize):
+    y = random.randint(0,1)
+    list.append(y)
+
+print(list)
+
+while ((packetnumer+1)*packetsize)-1 <= msgsize-1:
+    print(list[packetnumer*packetsize :  ((packetnumer+1)*packetsize)])
+    packetnumer += 1

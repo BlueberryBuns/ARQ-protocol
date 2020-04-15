046b6fd192addbe91fb919c99053396947d66a4f
Konrad-Abramowski <49274148+Konrad-Abramowski@users.noreply.github.com>
2020-04-15 20:02:01 +0200

    Delete Client.py

--- Client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect((socket.gethostname(), 1234))
-
-msg = clientsocket.recv(1024)
-print(msg.decode("utf-8"))

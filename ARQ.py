93c43fd28e5d4ca6b175a8bd407f15c6621e07da
Konrad <abramowski.konrad@gmail.com>
2020-04-16 16:03:16 +0200

    Message disturbance implemented

--- ARQ.py
+++ ARQ.py
@@ -1,14 +1,31 @@
 import random
-list = []
+
+msg = []
 msgsize = int(input("Podaj ilosc bitow do wyslania: "))
 packetsize = int(input("Podaj wielkosc pojedynczego pakietu: "))
+errorprob = int(input("Podaj prawdopodobienstwo wystapienia zaklocenia bitu wyrazone w % : "))
 packetnumer = int(0)
+
 for i in range(msgsize):
-    y = random.randint(0,1)
-    list.append(y)
+    rand = random.randint(0, 1)
+    msg.append(rand)
 
-print(list)
+print(f"Wiadomosc przed zakloceniem: {msg}")
+disturbedmsg = msg
 
-while ((packetnumer+1)*packetsize)-1 <= msgsize-1:
-    print(list[packetnumer*packetsize :  ((packetnumer+1)*packetsize)])
-    packetnumer += 1 #123
+for i in range(msgsize):
+    rand = random.randint(0, 100)
+    if errorprob >= rand:
+        if disturbedmsg[i] == 0:
+            disturbedmsg[i] = 1
+        else:
+            disturbedmsg[i] = 0
+
+print(f"Wiadomosc po zakloceniu    : {disturbedmsg}")
+
+
+while ((packetnumer + 1) * packetsize) - 1 <= msgsize - 1:
+    packet = [disturbedmsg[packetnumer * packetsize:  ((packetnumer + 1) * packetsize)]]
+    print(f"Pakiet nr {packetnumer+1} : {packet}")
+    packetnumer += 1
+

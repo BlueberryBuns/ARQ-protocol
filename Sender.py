6fdb207c4fbe08670dedb49bed2077e1f82735f8
Kamil Hulewicz <48947460+HulewiczKamil@users.noreply.github.com>
2020-04-24 23:57:19 +0200

    Started working on coding

--- Sender.py
+++ Sender.py
@@ -7,6 +7,7 @@ class Sender:
         self.sentData = []
         self.receivedAck = None
         self.packetSize = 8
+        self.encoding = None
 
     def makeData(self):
         for i in range(self.packetSize):
@@ -20,3 +21,18 @@ class Sender:
         for i in ["Wysłany pakiet:  " + str(i) for i in self.sentData]:
             print(i, b)
             b += 1
+
+    def xor(self, a, b):
+        for i in range(1, len(b)):
+            if a[i] != b[i]:
+                self.sentData.append('1')
+            else:
+                self.sentData.append('0')
+
+    def encodeCRC(self):
+        self.encoding = "CRC"
+        pass
+
+    def encodeParity(self):
+        self.encoding = "Parity"
+        pass

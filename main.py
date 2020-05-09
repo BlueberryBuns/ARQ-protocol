9b1616345e2a2a90819cfdcd7edacbd35a9b54b9
Kamil Hulewicz <hulewicz.k@gmail.com>
2020-05-09 17:55:48 +0200

    Extension and automatic test are to be implemented

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from Sender import Sender
 s1 = Sender()
 d1 = Dis()
 r1 = Receiver()
-
+#'''
+# CRC coding
 s1.makeData()
+s1.keyUpdate(r1)
 s1.updateData()
 s1.encodeCRC()
 s1.printData()
@@ -17,4 +19,22 @@ d1.distortPacket(s1)
 d1.drukuj()
 d1.passToReceiver(r1)
 r1.drukuj()
-r1.decodeCRC()
+r1.decodeCRC(s1)
+print(s1.receivedAck)
+#'''
+# parityBit
+'''
+s1.makeData()
+s1.encodeParity()
+s1.updateData()
+s1.printData()
+s1.SendData(d1)
+d1.drawErrorMsg()
+d1.drawSingleDigitError()
+d1.distortPacket(s1)
+d1.drukuj()
+d1.passToReceiver(r1)
+r1.decodeParityData(s1)
+r1.drukuj()
+print(s1.receivedAck)
+'''
